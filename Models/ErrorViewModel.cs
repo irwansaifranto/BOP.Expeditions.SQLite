867bc47ec77e6584ab3d6eb2099e8037d79ffeb0
toni firnandes <firnandestoni@gmail.com>
2018-02-23 11:59:19 +0700

    new mvc project BOP.Ekspedition

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BOP.Expedition.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
